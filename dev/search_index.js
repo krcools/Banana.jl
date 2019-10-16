091632d64babb660359ea3fab881258b17a5faf7
zeptodoctor <44736852+zeptodoctor@users.noreply.github.com>
2019-10-16 16:07:40 +0000

    build based on 1af040f

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"#Banana.jl-1","page":"Home","title":"Banana.jl","text":"","category":"section"},{"location":"#","page":"Home","title":"Home","text":"Docs for Banana!","category":"page"},{"location":"#","page":"Home","title":"Home","text":"","category":"page"},{"location":"#","page":"Home","title":"Home","text":"Modules = [Banana]","category":"page"},{"location":"#Base.insert!-Tuple{LinkedList,Any,Any}","page":"Home","title":"Base.insert!","text":"This is how you insert elements.\n\n\n\n\n\n","category":"method"}]
+}
